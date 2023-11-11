66c66a76c974ce684d7fc41555f908349484da2b
Mantas Petrauskas <mantasp147@gmail.com>
2023-11-11 22:55:31 +0200

    Added email change form without request.

--- src/pages/client/order-history/OrderHistoryPage.tsx
+++ src/pages/client/order-history/OrderHistoryPage.tsx
@@ -1,14 +1,13 @@
-import React from "react";
 import Footer from "../../../components/footer/Footer";
 import OrderHistoryUI from "../../../components/order-history-ui/OrderHistoryUI";
 
 const OrderHistoryPage = () => {
-    return (
-        <>
-            <OrderHistoryUI/>
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      <OrderHistoryUI />
+      <Footer />
+    </>
+  );
 };
 
 export default OrderHistoryPage;
